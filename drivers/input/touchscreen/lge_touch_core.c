28f89b52c71af063e1d47871cb4e4e2bd29bbe1e
codeworkx <codeworkx@cyanogenmod.org>
2016-03-12 09:25:02 +0100

    lge touch core: use KEY_WAKEUP for tap 2 wake
    Change-Id: I847488b98002a9b707a05bd73da8253bbc233581
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/input/touchscreen/lge_touch_core.c
+++ drivers/input/touchscreen/lge_touch_core.c
@@ -178,6 +178,13 @@ void send_uevent_lpwg(struct i2c_client *client, int type)
 			== UEVENT_IDLE) {
 		atomic_set(&ts->state.uevent, UEVENT_BUSY);
 		send_uevent(&client->dev, lpwg_uevent[type-1]);
+
+		if (type == LPWG_DOUBLE_TAP) {
+			input_report_key(ts->input_dev, KEY_WAKEUP, BUTTON_PRESSED);
+			input_sync(ts->input_dev);
+			input_report_key(ts->input_dev, KEY_WAKEUP, BUTTON_RELEASED);
+			input_sync(ts->input_dev);
+		}
 	}
 
 	return;
@@ -4220,6 +4227,8 @@ static int touch_probe(struct i2c_client *client,
 
 	set_bit(EV_SYN, ts->input_dev->evbit);
 	set_bit(EV_ABS, ts->input_dev->evbit);
+	set_bit(EV_KEY, ts->input_dev->evbit);
+	set_bit(KEY_WAKEUP, ts->input_dev->keybit);
 	set_bit(INPUT_PROP_DIRECT, ts->input_dev->propbit);
 
 	input_set_abs_params(ts->input_dev,
