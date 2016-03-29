17e222205b15ca155b72cddbf21ec802aef4acd7
Eliminater74 <eliminater74@gmail.com>
2016-03-28 22:00:03 -0400

    Fixed: Sweet:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1556,7 +1556,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			rc = mdss_dsi_unblank(pdata);
 		pdata->panel_info.esd_rdy = true;
 #ifdef CONFIG_STATE_NOTIFIER
-		if (!use_fb_notifier)
 			state_resume();
 #endif
 		break;
@@ -1577,7 +1576,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			rc = mdss_dsi_blank(pdata, power_state);
 		rc = mdss_dsi_off(pdata, power_state);
 #ifdef CONFIG_STATE_NOTIFIER
-		if (!use_fb_notifier)
 			state_suspend();
 #endif
 #if IS_ENABLED(CONFIG_LGE_DISPLAY_POWER_SEQUENCE)
